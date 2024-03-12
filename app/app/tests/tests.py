95842921176582dbd6b469a681c55faad8474c33
yawMD <emmamcdans@gmail.com>
2024-03-12 16:46:46 -0500

    fix(lint)

--- app/app/tests/tests.py
+++ app/app/tests/tests.py
@@ -10,19 +10,19 @@ class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
         """Test adding numbers together"""
-        a=2 
-        b=3
-        res = calc.add(a,b)
+        a = 2 
+        b = 3
+        res = calc.add(a, b)
         self.assertEqual(res, 5)
-        
+    
     def test_subtract_numbers(self):
         """Test subtracting numbers together"""
-        a=10 
-        b=3
-        res = calc.subtract(a,b)
+        a = 10 
+        b = 3
+        res = calc.subtract(a, b)
         self.assertEqual(res, 7)
 
     def test_divide_numbers(self):
-        #divides numbers
-        res = calc.divide(6,2)
+        # divides numbers
+        res = calc.divide(6, 2)
         self.assertEqual(res, 3)
